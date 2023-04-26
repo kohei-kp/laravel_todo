be93f3c2a37d209b74a2c120edc811d2ad98a176
kohei-kp <cowhey1991@gmail.com>
2023-04-26 10:11:42 +0900

    feat: Model, Controller生成とRouteの整備まで

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\AuthorCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/user', function () {
+    Route::get('/todos', [TodoController::class, 'index']);
+    Route::post('/todos', [TodoController::class, 'store']);
+    Route::put('/todos', [TodoController::class, 'update']);
+    Route::delete('/todos', [TodoController::class, 'destroy']);
+    Route::post('/todos', [TodoController::class, 'logout']);
+});
+
+Route::post('/login', [AuthorCollection::class, 'login']);
+
+Route::get('/version', function () {
+    return response()->json([
+        'app_version' => '0.0',
+        'laravel' => app()::VERSION,
+    ]);
 });
